41f6eefe9b807a0727ef1894791125b2fb4e5482
bilu abdi <biluabdi91@gmail.com>
2025-06-08 08:17:40 +0300

    Complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// 1. scuberGreetingForFeet
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return "This one is on me!";
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// 2. ternaryCheckCity
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// 3. switchOnCharmFromTip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
